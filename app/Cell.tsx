ecad48c947a10bae643a69ff2ddee32123b88156
Thomas Harris <loden16harris@gmail.com>
2024-03-14 22:08:12 -0500

    it places and alternates letters

--- app/Cell.tsx
+++ app/Cell.tsx
@@ -8,21 +8,36 @@ interface cellProps {
   handleCellClick: (id: string) => void;
 }
 
+
 export default function GridCell({
   cell: { id, selected, letter },
   handleCellClick,
 }: cellProps) {
+
+  const determineCharacter = () => {
+    switch (letter) {
+      case "x":
+        return <span>&#10005;</span>;
+      case "o":
+        return <span>&#9900;</span>;
+      case null:
+        return null;
+    }
+  };
+
   return (
-    <div
+    <button
       id={id}
       key={`cell-${id}`}
+      disabled={selected}
       className={
         "size-full rounded-md " +
+        "flex items-center text-center " +
         (selected ? "bg-slate-400" : "bg-slate-300 hover:bg-slate-400")
       }
       onClick={() => handleCellClick(id)}
     >
-      {letter}
-    </div>
+      <span className="w-full text-7xl font-mono">{determineCharacter()}</span>
+    </button>
   );
 }
