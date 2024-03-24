ad02e086d38bfb1bdec25608a70411bb13f7f408
Thomas Harris <loden16harris@gmail.com>
2024-03-24 16:16:48 -0500

    functions to check if game has been won

--- app/gameContextStuff.ts
+++ app/gameContextStuff.ts
@@ -2,8 +2,10 @@ import { createContext } from "react";
 
 const x = "x";
 const o = "o";
+
 export const xStartsFirst = [x, o, x, o, x, o, x, o, x];
 export const oStartsFirst = [o, x, o, x, o, x, o, x, o];
+
 // Helpers for game state
 export interface GameStateType {
   startingTurn: string;
@@ -33,6 +35,87 @@ export const processUserTurn = (
   newGameState.isUserTurn = !newGameState.isUserTurn;
 
   return newGameState;
+};
+
+// HELPERS FOR CHECKING ENDGAME
+interface WinState {
+  hasWon: boolean;
+  winner: string | null;
+  winningRow: number[];
+}
+
+const winningHorizontalRows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const winningVerticalRows = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+
+const winningDiagonalRows = [
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const possibleWinningRows = [
+  ...winningHorizontalRows,
+  ...winningVerticalRows,
+  ...winningDiagonalRows,
+];
+
+const xWins = JSON.stringify([x, x, x]);
+const oWins = JSON.stringify([o, o, o]);
+
+// get values of cells that could possibly be winning rows
+const getLettersFromCells = (
+  cellState: GridCellType[],
+  indexesToGrab: number[]
+) => indexesToGrab.map((i: number) => cellState[i].letter);
+
+// compare row of cells for win state
+const compareRows = (cellRow: string[]) => {
+  const stringifiedCellRow = JSON.stringify(cellRow);
+  if (stringifiedCellRow === xWins) return x;
+  if (stringifiedCellRow === oWins) return o;
+  return null;
+};
+
+// check each possible winning row for win state
+export const checkEndGame = (cellState: GridCellType[]): WinState => {
+  let winState: WinState = {
+    hasWon: false,
+    winner: null,
+    winningRow: [],
+  };
+
+  // for each possible winning row of indices...
+  for (let possibleRow of possibleWinningRows) {
+    // ...assimilate the letters it corresponds to
+    const cellRow: Array<string | null> = getLettersFromCells(
+      cellState,
+      possibleRow
+    );
+
+    // if row's cells aren't filled don't bother checking it
+    if (cellRow.includes(null)) continue;
+
+    // compare row of letters to xWins and oWins
+    const winner = compareRows(cellRow as string[]);
+
+    // return win state if someone won
+    if (winner) {
+      winState.hasWon = true;
+      winState.winner = winner;
+      winState.winningRow = possibleRow;
+      // stop checking if someone wins
+      break;
+    }
+  }
+  // return winState
+  return winState;
 };
 
 // Helpers for cells
