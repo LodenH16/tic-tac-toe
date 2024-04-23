3976c08696e0a8a6bec2b4d090e5f29a53c92a6f
Thomas Harris <loden16harris@gmail.com>
2024-04-23 13:48:48 -0500

    remove data-testId props from prod because it throws a nasty error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties: {
+      properties: ["data-testId"],
+    },
+  },
+};
 
 export default nextConfig;
